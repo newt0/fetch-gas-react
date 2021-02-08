d663f880f62706fc1ae056298c59ea2d4bf5bdb7
newt0 <py_lambda@yahoo.co.jp>
2021-02-08 18:40:49 +0900

    add: fetch data from googlesheets by useEffect, and store it by useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import React, { useState, useEffect, Fragment } from "react";
+import Tabletop from "tabletop";
 
 export const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1XrMZqr4aL_XbP5ULfzvMg1a6G2GUOjpmsi1Rr0JviAw", // Hide it later.
+      simpleSheet: true,
+    })
+      .then((data) => setData(data))
+      .catch((err) => console.warn(err)); // Fix it later.
+  }, []);
+
+  console.log("retrieved data>>>", data); // Delete it later.
+
   return <div className="app"></div>;
 };
 
